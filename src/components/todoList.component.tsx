e5f566bef09098f816a0a2d469c01db34afd1f39
Christopher Stewart <christopher.stewart@slalom.com>
2023-03-17 13:16:12 -0700

    added a couple notes to the readme

--- src/components/todoList.component.tsx
+++ src/components/todoList.component.tsx
@@ -5,10 +5,17 @@ import { StyleSheet } from 'react-native';
 import { ListRenderItemInfo } from 'react-native';
 import { useSelector } from 'react-redux';
 import { TodoSliceObject } from '@store';
+import { removeTodo } from '@store';
+import { MyButton } from './myButton.component';
+import { useDispatch } from 'react-redux';
+import Realm from 'realm';
 
 const { useQuery } = TodoRealmContext;
 
 export const TodoList = () => {
+  const { useRealm } = TodoRealmContext;
+  const realm = useRealm();
+  const dispatch = useDispatch();
   const realmData = useQuery(Todo);
   const reduxDate: TodoSliceObject[] = useSelector(
     (state: any) => state.todos.todos,
@@ -24,15 +31,37 @@ export const TodoList = () => {
     return;
   };
 
+  function handleRemoveTodo(todo) {
+    const id = new Realm.BSON.ObjectId();
+    dispatch(
+      removeTodo({
+        id: todo.id,
+        name: todo.name,
+      }),
+    );
+    console.log('redux remove', todo.name, id);
+    realm.write(() => {
+      console.log(todo.id);
+      realm.delete(realm.objectForPrimaryKey('Todo', todo.id));
+      // realm.delete({todo});
+      todo = null;
+    });
+  }
+
   const Item = ({
     item,
   }: ListRenderItemInfo<Todo & Realm.Object<unknown, never>>) => {
     return (
-      <ListItem
-        title={<Text>{item.name}</Text>}
-        description={<Text>{item._id.toHexString()}</Text>}
-        accessoryRight={isReduxAccessory(item._id.toHexString())}
-      />
+      <>
+        <ListItem
+          title={<Text>{item.name}</Text>}
+          accessoryLeft={
+            <MyButton text={'Remove Todo'} buttonHandler={handleRemoveTodo} />
+          }
+          description={<Text>{item._id.toHexString()}</Text>}
+          accessoryRight={isReduxAccessory(item._id.toHexString())}
+        />
+      </>
     );
   };
 
